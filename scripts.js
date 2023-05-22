e197f9b1cfb52d232484918fef36831eed81a41d
João Luccas Marques <107486816+JoaoLu2cas@users.noreply.github.com>
2023-05-22 09:56:52 -0300

    obrigado amigo

--- scripts.js
+++ scripts.js
@@ -13,6 +13,6 @@ nop.addEventListener("mouseover", moveNot);
 let yep = document.querySelector(".yesBtn");
 
 yep.addEventListener("click", function() {
-  window.location.href = "https://www.youtube.com/watch?v=orWnzqBA63w" // the office version
+  window.location.href = "https://www.youtube.com/watch?v=ctOpSn3Xc4k" // the office version
   // window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" // Rick Astley (Never Gonna Give You Up ) version
-})
+}) // https://www.youtube.com/watch?v=orWnzqBA63w //
